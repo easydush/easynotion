eadf01a22ac329d6317a13a0dde4f2742df2993b
easydush <easydush@gmail.com>
2021-10-28 16:02:36 +0300

    added Modal

---
+++ src/stories/Modal.stories.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Modal } from '../components/Modal';
+
+
+export default {
+  title: 'Example/Modal',
+  component: Modal,
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => {
+  const [isVisible, switchVisible] = useState(true);
+
+  return (
+    <Modal visible={isVisible} content={<div>Content</div>} title={'Modal'} onClose={()=>switchVisible(false)}/>
+  )
+}
+
+export const Basic = Template.bind({});
+Basic.args = {};
