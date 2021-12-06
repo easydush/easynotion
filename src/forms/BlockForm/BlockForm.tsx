586f4fddf8c2d6b2a50929b5ffce741e9a43c060
easydush <easydush@gmail.com>
2021-12-06 12:28:08 +0300

    fixed block type select

--- src/forms/BlockForm/BlockForm.tsx
+++ src/forms/BlockForm/BlockForm.tsx
@@ -1,8 +1,8 @@
-import { useContext, useRef, useState } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { MediaType, Note } from '../../types';
+import { MediaType, Note } from 'types';
 import { Switcher } from './components/Switcher';
-import { create } from '../../store/actions/block';
+import { create } from 'store/actions/block';
 import cuid from 'cuid';
 
 type BlockForm = {
@@ -18,8 +18,6 @@ export const BlockForm = ({ type, noteId }: BlockForm) => {
     // TODO: replace order
     dispatch(create({ id: cuid(), noteId: noteId, type: type, content: content, order: 0 }));
   }
-
-  console.log(type);
 
   return <form onSubmit={handleFormSubmit}>
     <Switcher type={type} onChange={setContent} />
--- src/scenes/Dashboard/scenes/NoteView/components/TypeSwitcher/TypeSwitcher.tsx
+++ src/scenes/Dashboard/scenes/NoteView/components/TypeSwitcher/TypeSwitcher.tsx
@@ -18,11 +18,9 @@ type Option = {
 export const TypeSwitcher = ({ onChange, defaultValue }: TypeInputProps) => {
   const options = convertString2SelectOption(TYPES);
   const initial = { value: defaultValue, label: String(defaultValue).toLowerCase() };
-  const [selectedOption, setSelectedOption] = useState<Option>(initial);
 
   const handleChange = (option: any) => {
-    setSelectedOption(option);
-    onChange((selectedOption as Option).value);
+    onChange((option as Option).value);
   };
 
   return <Select defaultValue={initial} onChange={handleChange} options={options} />;
